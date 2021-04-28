6f7266d1b71e2da4a096387a2533cd02fd1f18f0
Ron Paolo Toyhacao <rptoyhacao@gmail.com>
2021-04-28 23:34:31 +0800

    Add task function added

---
+++ assets/scripts/app.js
@@ -0,0 +1,32 @@
+const taskInput = document.querySelector('.task-input');
+const taskButton = document.querySelector('.task-button');
+const taskList = document.querySelector('.task-list');
+
+taskButton.addEventListener('click', addTask);
+
+function addTask(e) {
+    e.preventDefault();
+
+    // add task: create list item
+    const newTask = document.createElement('li');
+    const newTaskLabel = document.createElement('label');
+    newTaskLabel.classList.add('container');
+    newTaskLabel.innerText = 'wew';
+    newTask.appendChild(newTaskLabel);
+
+    // add task: create checkbox
+    const inputCheckbox = document.createElement('input');
+    inputCheckbox.setAttribute('type', 'checkbox');
+    newTaskLabel.appendChild(inputCheckbox);
+    const completeTaskCheckbox = document.createElement('span');
+    completeTaskCheckbox.classList.add('checkmark');
+    newTaskLabel.appendChild(completeTaskCheckbox);
+
+    // add task: create delete button
+    const deleteTask = document.createElement('button');
+    deleteTask.classList.add('delete-btn');
+    deleteTask.innerHTML = '<i class="fas fa-times"></i>';
+    newTask.appendChild(deleteTask);
+
+    taskList.appendChild(newTask);
+}
